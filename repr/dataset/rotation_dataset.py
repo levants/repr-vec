8f8ca84808c7fb86bc2a4965ad3e02fa110715b5
levan <levantsinadze@gmail.com>
2019-07-26 16:08:52 +0400

    Adding rotation classifier self-supervised learning

---
+++ repr/dataset/rotation_dataset.py
@@ -0,0 +1,231 @@
+"""
+Created on Jul 25, 2019
+
+Data utility module for rotation classifier
+
+@author: Levan Tsinadze
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+from fastai.vision import *
+
+# Extensions
+_IMG_EXTS_1 = ['.jpg', '.jpeg', '.png']
+_IMG_EXTS = set(_IMG_EXTS_1 + [ext.upper() for ext in _IMG_EXTS_1])
+
+# Rotation angles
+_CLASSES = [0, 90, 180, 270]
+
+
+def init_transforms(size):
+    """
+    Initializes additional transforms
+    Args:
+        size: size of input image
+
+    Returns:
+        additional transformations
+    """
+    return rand_resize_crop(size) + [rand_crop(p=1.), crop_pad(), brightness(change=(0.1, 0.9), p=1.0),
+                                     contrast(scale=(0.5, 2.), p=1.), jitter(magnitude=-0.2, p=1.),
+                                     symmetric_warp(magnitude=(-0.2, 0.2), p=1.),
+                                     zoom(scale=1.78, p=1.), cutout(n_holes=(1, 4), length=(10, 160), p=1.),
+                                     squish(scale=(0.68, 1.38))]
+
+
+def make_dirs(dst_dir: Path) -> dict:
+    """
+    Make class directories
+    Args:
+        dst_dir: root destination directory
+
+    """
+    dest_dirs = dict()
+    for cl in _CLASSES:
+        dr = dst_dir / str(cl)
+        dr.mkdir(exist_ok=True)
+        dest_dirs[str(cl)] = dr
+
+    return dest_dirs
+
+
+def _init_rotation_matrix(h: int = 224, w: int = 224, tr_mx: dict = None) -> dict:
+    """
+    Rotation matrices
+    Args:
+        h: height of input
+        w: width of input
+        tr_mx: existing transformation (rotation matrices)
+
+    Returns:
+        ms: rotation matrices
+    """
+    if tr_mx is None:
+        center = (w / 2, h / 2)
+        ms = {str(cl): cv2.getRotationMatrix2D(center, cl, 1) for cl in _CLASSES if cl != 0}
+        ms['0'] = None
+    else:
+        ms = tr_mx
+
+    return ms
+
+
+def _read_resize(p: str, h: int, w: int, interpolation: int) -> np.ndarray:
+    """
+    Read and resize image
+    Args:
+        p: path to image
+        h: height to resize
+        w: width to resize
+        interpolation: interpolation for resize
+
+    Returns:
+        resized image
+    """
+    return cv2.resize(cv2.imread(str(p), cv2.IMREAD_ANYCOLOR), (w, h), interpolation=interpolation)
+
+
+def generate_classes(src_dir: Path, dst_dir: Path, h: int = 224, w: int = 224, tr_mx: dict = None,
+                     interpolation: int = cv2.INTER_LINEAR):
+    """
+    Generate rotation dataset
+    Args:
+        src_dir: source directory
+        dst_dir: destination directory
+        h: height of image
+        w: width of image
+        tr_mx: rotation matrices
+        interpolation: interpolation for resize
+
+    """
+    dest_dirs = make_dirs(dst_dir)
+    img_dict = {str(p.name): _read_resize(p, h, w, interpolation) for p in src_dir.iterdir() if p.suffix in _IMG_EXTS}
+    ms = _init_rotation_matrix(h=h, w=w, tr_mx=tr_mx)
+    for k, v in img_dict.items():
+        for d, m in ms.items():
+            mod = v if d == '0' else cv2.warpAffine(v, m, (h, w))
+            dst = dest_dirs[d]
+            cv2.imwrite(str(dst / k), mod)
+
+
+def _add_tests(src_root: Path, dst_root: Path, src_dirs: list, dst_dirs: list, tst_dir: str = None):
+    """
+    Add test directory to source and destination paths
+    Args:
+        src_root: source root
+        dst_root: destination root
+        src_dirs: source directories
+        dst_dirs: destination directories
+        tst_dir: test directory name
+
+    """
+    if tst_dir:
+        src_dirs += [src_root / tst_dir]
+        dst_dirs += [dst_root / tst_dir]
+
+
+def generate_data(src_root: Path, dst_root: Path, h: int = 224, w: int = 224, tr_dir: str = 'train',
+                  val_dir: str = 'valid', tst_dir: str = None):
+    """
+    Generate rotation data-set
+    Args:
+        src_root: source root directory
+        dst_root: destination root directory
+        h: height of image
+        w: width of image
+        tr_dir: training directory
+        val_dir: validation directory
+        tst_dir: test directory
+
+    """
+    src_dirs = [src_root / tr_dir, src_root / val_dir]
+    dst_dirs = [dst_root / tr_dir, dst_root / val_dir]
+    _add_tests(src_root, dst_root, src_dirs, dst_dirs, tst_dir=tst_dir)
+    tr_mx = _init_rotation_matrix(h=h, w=w)
+    for idx, src_dir in enumerate(src_dirs):
+        if src_dir.exists():
+            dst_dir = dst_dirs[idx]
+            dst_dir.mkdir(exist_ok=True)
+            generate_classes(src_dir, dst_dir, h=h, w=w, tr_mx=tr_mx)
+
+
+def label_and_folder(src_root: Path, dst_root: Path, h: int = 224, w: int = 224, train: PathOrStr = 'train',
+                     valid: PathOrStr = 'valid', valid_pct=None):
+    """
+    Label and put data in folders
+    Args:
+        src_root: source root directory
+        dst_root: destination root directory
+        h: height of image
+        w: width of image
+        train: training directory
+        valid: validation directory
+        valid_pct: validation percentage
+
+    """
+    if valid_pct is None:
+        generate_data(src_root, dst_root, h=h, w=w, tr_dir=train, val_dir=valid)
+    else:
+        generate_classes(src_root, dst_root, h=h, w=w, tr_mx=None)
+
+
+def init_databunch(dst_root: Path, train: PathOrStr = 'train', valid: PathOrStr = 'valid', valid_pct=None,
+                   seed: int = None, size: int = None, classes: Collection = None, bs: int = 64, val_bs: int = None,
+                   ds_tfms: Optional[TfmList] = None, **kwargs: Any) -> ImageDataBunch:
+    """
+    Initializes image data bunch
+    Args:
+        dst_root: destination root directory
+        train: training directory
+        valid: validation directory
+        valid_pct: validation percentage
+        seed: random seed
+        size: input size
+        classes: classes for training
+        bs: batch size
+        val_bs: validation batch size
+        ds_tfms: data transformation
+        **kwargs: additional arguments
+
+    Returns:
+        initialized image data bunch
+    """
+    return ImageDataBunch.from_folder(dst_root, train=train, valid=valid, valid_pct=valid_pct, seed=seed,
+                                      classes=classes, bs=bs, val_bs=val_bs, size=size, ds_tfms=ds_tfms, **kwargs)
+
+
+def rotation_databunch(src_root: Path, dst_root: Path, h: int = 224, w: int = 224, size: int = None,
+                       train: PathOrStr = 'train', valid: PathOrStr = 'valid', valid_pct=None, seed: int = None,
+                       classes: Collection = None, bs: int = 64, val_bs: int = None, ds_tfms: Optional[TfmList] = None,
+                       **kwargs: Any) -> ImageDataBunch:
+    """
+    Split data to directories
+    Args:
+        src_root: source root directory
+        dst_root: destination root directory
+        h: height of image
+        w: width of image
+        size: input size
+        train: training directory
+        valid: validation directory
+        valid_pct: validation percentage
+        seed: random seed
+        classes: classes for training
+        bs: batch size
+        val_bs: validation batch size
+        ds_tfms: data transformation
+        **kwargs: additional arguments
+
+    Returns:
+        data_bunch: image data bunch for training / validation
+    """
+    dst_root.mkdir(exist_ok=True)
+    label_and_folder(src_root, dst_root, h=h, w=w, train=train, valid=valid, valid_pct=valid_pct)
+    im_size = (h + w) / 2 if size is None else size
+    data_bunch = init_databunch(dst_root, train=train, valid=valid, valid_pct=valid_pct, seed=seed, size=im_size,
+                                classes=classes, bs=bs, val_bs=val_bs, ds_tfms=ds_tfms, **kwargs)
+
+    return data_bunch
---
+++ repr/models/resnet.py
@@ -0,0 +1,261 @@
+"""
+Created on Jul 12, 2019
+
+Implementation of ResNet models with adaptive average pooling (image resizing tolerant)
+for fine-tuning, transfer learning, features extractor etc
+
+@author: Levan Tsinadze
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from fastai.vision import *
+from torch.utils import model_zoo
+from torchvision.models.resnet import (ResNet, Bottleneck, BasicBlock, model_urls)
+
+__all__ = ['Flatten', 'ResNetModule', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152', 'init_model']
+
+
+class Flatten(nn.Module):
+    """Flatten input tensor to vector"""
+
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class ResNetModule(ResNet):
+    """ResNet extension model"""
+
+    def __init__(self, block, layers, channels=3, num_classes=1000):
+        super(ResNetModule, self).__init__(block, layers, num_classes=num_classes)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.conv1 = nn.Conv2d(channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.flatten = Flatten()
+
+    def features(self, input_tensor):
+        """
+        Extracts features from input tensor
+        Args:
+            input_tensor: input image tensor
+
+        Returns:
+            features_tensor: features tensor
+        """
+        x = self.conv1(input_tensor)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        # Residual layers
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        features_tensor = self.layer4(x)
+
+        return features_tensor
+
+    def glob_pool(self, input_tensor):
+        """
+        Extracts features from average pooling layer of input tensor
+        Args:
+            input_tensor: input image tensor
+
+        Returns:
+            features_tensor: features tensor
+        """
+        x = self.features(input_tensor)
+        features_tensor = self.avgpool(x)
+
+        return features_tensor
+
+    def vect(self, input_tensor):
+        """
+        Extracts features from average pooling layer of input tensor
+        Args:
+            input_tensor: input image tensor
+
+        Returns:
+            features_tensor: features tensor
+        """
+        x = self.glob_pool(input_tensor)
+        features_tensor = self.flatten(x)
+
+        return features_tensor
+
+    def forward(self, input_tensor):
+        x = self.vect(input_tensor)
+        output_tensor = self.fc(x)
+
+        return output_tensor
+
+
+def _init_layers(layers):
+    """
+    Sets default values to layers
+    Args:
+        layers: layers for ResNet module
+
+    Returns:
+        default value if layers are not defined
+    """
+    return [2, 2, 2, 2] if layers is None else layers
+
+
+def _init_model(core_type=ResNetModule, block=BasicBlock, layers=None,
+                model_key='resnet18', pretrained=False, **kwargs):
+    """
+    Initializes appropriated model
+    Args:
+        core_type: type for model core initialization
+        block: block for layers initialization
+        layers: model layers
+        model_key: key for model URL dictionary
+        pretrained: flag for trained weights
+        **kwargs: additional arguments
+
+    Returns:
+        model: network model with pre-trained weights
+    """
+    model = core_type(block, _init_layers(layers), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls[model_key]))
+
+    return model
+
+
+def _init_module(block=BasicBlock, layers=None, model_key='resnet18',
+                 pretrained=False, **kwargs):
+    """
+    Initializes appropriated model
+    Args:
+        block: block for layers initialization
+        layers: model layers
+        model_key: model architecture
+        pretrained: flag for trained weights
+        **kwargs: additional arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_model(core_type=ResNetModule, block=block, layers=layers,
+                       model_key=model_key, pretrained=pretrained, **kwargs)
+
+
+def resnet18(pretrained=False, **kwargs):
+    """
+    Constructs a ResNet-18 model.
+    Args:
+        pretrained: If True, returns a model pre-trained on ImageNet data
+        **kwargs: additional named arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_module(block=BasicBlock, layers=[2, 2, 2, 2], model_key=resnet18.__name__,
+                        pretrained=pretrained, **kwargs)
+
+
+def resnet34(pretrained=False, **kwargs):
+    """
+    Constructs a ResNet-34 model.
+    Args:
+        pretrained: If True, returns a model pre-trained on ImageNet data
+        **kwargs: additional named arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_module(block=BasicBlock, layers=[3, 4, 6, 3], model_key=resnet34.__name__,
+                        pretrained=pretrained, **kwargs)
+
+
+def resnet50(pretrained=False, **kwargs):
+    """
+    Constructs a ResNet-50 model.
+    Args:
+        pretrained: If True, returns a model pre-trained on ImageNet data
+        **kwargs: additional named arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_module(block=Bottleneck, layers=[3, 4, 6, 3], model_key=resnet50.__name__,
+                        pretrained=pretrained, **kwargs)
+
+
+def resnet101(pretrained=False, **kwargs):
+    """
+    Constructs a ResNet-101 model.
+    Args:
+        pretrained: If True, returns a model pre-trained on ImageNet data
+        **kwargs: additional named arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_module(block=Bottleneck, layers=[3, 4, 23, 3], model_key=resnet101.__name__,
+                        pretrained=pretrained, **kwargs)
+
+
+def resnet152(pretrained=False, **kwargs):
+    """
+    Constructs a ResNet-152 model.
+    Args:
+        pretrained: If True, returns a model pre-trained on ImageNet data
+        **kwargs: additional named arguments
+
+    Returns:
+        network model with pre-trained weights
+    """
+    return _init_module(block=Bottleneck, layers=[3, 8, 36, 3], model_key=resnet152.__name__,
+                        pretrained=pretrained, **kwargs)
+
+
+def _name_2_func(arch_name: str) -> callable:
+    """
+    Extracts callable by name
+    Args:
+        arch_name: architecture name
+
+    Returns:
+        arch_func: architecture callable
+    """
+    if arch_name == resnet18.__name__:
+        arch_func = resnet18
+    elif arch_name == resnet34.__name__:
+        arch_func = resnet34
+    elif arch_name == resnet50.__name__:
+        arch_func = resnet50
+    elif arch_name == resnet101.__name__:
+        arch_func = resnet101
+    elif arch_name == resnet152.__name__:
+        arch_func = resnet152
+    else:
+        arch_func = resnet34
+
+    return arch_func
+
+
+def init_model(arch, pretrained=False, head: nn.Sequential = None, **kwargs):
+    """
+    Initializes ResNet model
+    Args:
+        arch: architecture callable
+        pretrained: pre-trained model
+        head: custom header
+        **kwargs: additional arguments
+
+    Returns:
+        model: initialized model
+    """
+    model_func = _name_2_func(arch) if isinstance(arch, str) else _name_2_func(arch.__name__)
+    if head is None:
+        model = model_func(pretrained=pretrained, **kwargs)
+    else:
+        body = create_body(model_func, pretrained=pretrained)
+        model = nn.Sequential(body, head)
+
+    return model
